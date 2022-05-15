5f2b8e425fa8c637f6473dad4508fba5fe81c053
Carly <Cvberquam@gmail.com>
2022-05-15 15:20:19 -0700

    Added API icon

--- src/app.js
+++ src/app.js
@@ -18,6 +18,7 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
@@ -25,6 +26,12 @@ function displayTemperature(response) {
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute("src",
+  `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  
+  iconElement.setAttribute("alt",
+  response.data.weather[0].description);
+
 }
 
 let apiKey = "e937ae3f7a274820b678821bd8a9635d";
