6ae3118b261b9cc7150f07fd5a65e4969be09664
Carly <Cvberquam@gmail.com>
2022-05-15 12:59:19 -0700

    Created current time, fixed line html-34

--- src/app.js
+++ src/app.js
@@ -1,16 +1,30 @@
+function formatDate(timestamp) {
+let date = new Date(timestamp);
+let hours = date.getHours();
+let minutes = date.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let day = days[date.getDay()];
+
+return `${day} ${hours}:${minutes}`
+}
+
 function displayTemperature(response) {
-  console.log(apiUrl);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.queryCommandIndeterm("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "e937ae3f7a274820b678821bd8a9635d";
