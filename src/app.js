ef564a1f0bd256d6e1522bfbee40894e61b3df35
Carly <Cvberquam@gmail.com>
2022-05-22 18:29:20 -0700

    Set up JS for forecast

--- src/app.js
+++ src/app.js
@@ -10,7 +10,15 @@ let day = days[date.getDay()];
 
 return `${day} ${hours}:${minutes}`;
 }
-function displayForecast() {
+
+function getForecast(coordinates) {
+  let apiKey = "e937ae3f7a274820b678821bd8a9635d";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
+function displayForecast(resposne) {
+  console.log(resposne.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
@@ -55,8 +63,9 @@ function displayTemperature(response) {
   iconElement.setAttribute("src",
   `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
   
-  iconElement.setAttribute("alt",
-  response.data.weather[0].description);
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -100,5 +109,3 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
-
-displayForecast();
