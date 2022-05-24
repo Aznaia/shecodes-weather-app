3bf1b95ca7ff552abf423b89c9aae15fe229f15f
Carly <Cvberquam@gmail.com>
2022-05-23 20:49:08 -0700

    Shortened forecast day names & edited styling

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ return `${day} ${hours}:${minutes}`;
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
-  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
   return days[day];
 }
@@ -35,7 +35,7 @@ forecast.forEach(function (forecastDay, index) {
   if (index < 6) {
   forecastHTML = forecastHTML +
   `
-  <div class="col-2">
+  <div class="col-2 card">
     <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
     <img 
     src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png
